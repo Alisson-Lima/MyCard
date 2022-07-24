a28abd6a7656f6d82aa8ec6ab3646a21588ab3e3
Alisson Renan <renalima.rl46@gmail.com>
2022-07-24 15:36:06 -0300

    Correcting instagram URL

--- src/components/Social/index.jsx
+++ src/components/Social/index.jsx
@@ -4,7 +4,7 @@ export function Social(){
     return(
         <div className="socialContainer">
             <a href="https://github.com/Alisson-Lima" target='_blank'><IconsSOcial icon={2} /></a>
-            <a href="https://www.instagram.com/alissonb.lima/" target='_blank'><IconsSOcial icon={3} /></a>
+            <a href="https://www.instagram.com/alissondofront/" target='_blank'><IconsSOcial icon={3} /></a>
             <a href="https://www.linkedin.com/in/alisson-renan-537712234/" target='_blank'><IconsSOcial icon={4} /></a>
         </div>
     )
